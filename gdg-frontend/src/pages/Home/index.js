33567c6256f9cbe05c0b20fa9a54deb598957554
Mathews Motta <mathewscsmotta@gmail.com>
2020-05-11 22:03:39 +0100

    Add some base text to first page

--- gdg-frontend/src/pages/Home/index.js
+++ gdg-frontend/src/pages/Home/index.js
@@ -1,9 +1,33 @@
 import React from 'react'
+import Jump from 'react-reveal/Jump';
+import Fade from 'react-reveal/Fade';
 
 import './styles.css'
 
+// import gamedevImg from '../../assets/gamedev.png'
+
 export default function Home(){
     return (
-        <h1>Home Page</h1>
+        <div className="home-container">
+            <section className="form">
+                <div className="logo-text">
+                    <Jump>
+                        <h1>GameDev Galático</h1>
+                    </Jump>
+                </div>
+                <div>
+                    <Fade bottom>
+                        <p>Aprenda a fazer jogos com guias de altíssima qualidade! Tudo sobre design, programação e design de jogos.</p>
+                    </Fade>
+                </div>
+                
+                <form action="">
+                    <h1>Receba um E-Book Grátis e aprenda hoje programação e criação de jogos no Unity!</h1>
+                    <input placeholder="Seu E-mail" />
+                    <button type="submit">Receber E-Book!</button>
+                </form>
+            </section>
+        </div>
+        
     )
 }
