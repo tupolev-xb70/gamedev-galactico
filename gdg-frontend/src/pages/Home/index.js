066eae178e1966d711f9adf6d7ce5334a1b35004
Mathews Motta <mathewscsmotta@gmail.com>
2020-05-11 22:36:04 +0100

    more styling to home

--- gdg-frontend/src/pages/Home/index.js
+++ gdg-frontend/src/pages/Home/index.js
@@ -15,17 +15,22 @@ export default function Home(){
                         <h1>GameDev Galático</h1>
                     </Jump>
                 </div>
-                <div>
+                <div className="desc-text">
                     <Fade bottom>
                         <p>Aprenda a fazer jogos com guias de altíssima qualidade! Tudo sobre design, programação e design de jogos.</p>
                     </Fade>
                 </div>
                 
-                <form action="">
-                    <h1>Receba um E-Book Grátis e aprenda hoje programação e criação de jogos no Unity!</h1>
-                    <input placeholder="Seu E-mail" />
-                    <button type="submit">Receber E-Book!</button>
+                <form>
+                    <Fade bottom>
+                        <h1 className="form-desc">Receba um E-Book Grátis</h1>
+                        <input placeholder="Seu E-mail" />
+                        <button type="submit" className="button">Receber E-Book!</button>
+                    </Fade>
                 </form>
+            </section>
+            <section className="cursos">
+
             </section>
         </div>
         
