ce92bc633c0c41360c877ddc08569fee18be86ba
Mathews Motta <mathewscsmotta@gmail.com>
2020-07-26 20:40:08 +0100

    navigation bar added

--- gdg-frontend/src/App.js
+++ gdg-frontend/src/App.js
@@ -1,12 +1,34 @@
-import React from 'react'
+import React, { useState, useRef }  from 'react'
+import { useOnClickOutside } from './hooks';
+import FocusLock from 'react-focus-lock';
 
 import './global.css'
+import { ThemeProvider } from 'styled-components';
+//import { GlobalStyles } from './global';
+import { Burger, Menu } from './components';
+import { theme } from './theme';
 
 import Home from './pages/Home'
 
 function App() {
+  const [open, setOpen] = useState(false);
+  const node = useRef();
+  const menuId = "main-menu";
+
+  useOnClickOutside(node, () => setOpen(false));
+
   return (
-    <Home />
+    <ThemeProvider theme={theme}>
+      <div ref={node}>
+        <FocusLock disabled={!open}>
+          <Burger open={open} setOpen={setOpen} aria-controls={menuId} />
+          <Menu open={open} setOpen={setOpen} id={menuId} />
+        </FocusLock>
+      </div>
+      <div>
+        < Home />
+      </div>
+    </ThemeProvider>
   );
 }
 
--- gdg-frontend/src/pages/Home/index.js
+++ gdg-frontend/src/pages/Home/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Jump from 'react-reveal/Jump';
 import Fade from 'react-reveal/Fade';
 
@@ -8,29 +8,47 @@ import logo from '../../assets/gameDevGalacticoNome.png'
 // import gamedevImg from '../../assets/gamedev.png'
 
 export default function Home(){
+    const [email, setEmail] = useState('');
+
+    function handleSubmit(e){
+        e.preventDefault();
+
+        window.emailjs.send(
+            'default_service',
+            'template_hWbVKn93', // email templateId
+            {
+                email //receiver
+            },
+            'user_LNYJDXD8PVeA8CZzTQUuD'
+        ).then(res => {
+            alert('Anotado! Um e-mail será enviado para você em breve.')
+        }).catch(err => console.error('Failed to send email. Error: ', err))
+        
+    }
+
     return (
         <div className="home-container">
             <section className="form">
                 <div className="logo-text">
                     <Jump>
-                        <img src={logo} alt="GameDev Galáctico"/>
-                        {/* <h1>GameDev Galáctico</h1> */}
+                        {/* <img src={logo} alt="GameDev Galáctico"/> */}
+                        <h1>GameDev Galáctico</h1>
                     </Jump>
                 </div>
                 <div className="desc-text">
                     <Fade bottom>
-                        <p>Aprenda a fazer jogos com guias de altíssima qualidade!</p> 
-                        <p>Tudo sobre design, programação e design de jogos.</p>
+                        <p>Guias, cursos e</p> 
+                        <p>tudo sobre criação de jogos</p>
                     </Fade>
                 </div>
                 
-                {/* <form>
+                <form onSubmit={handleSubmit}>
                     <Fade bottom>
-                        <h1 className="form-desc">Receba um E-Book Grátis</h1>
-                        <input placeholder="Seu E-mail" />
-                        <button type="submit" className="button">Receber E-Book!</button>
+                        <h1 className="form-desc">Deixe seu e-mail para ficar sabendo de novidades</h1>
+                        <input placeholder="Seu E-mail" value={email} onChange={e => setEmail(e.target.value)}/>
+                        <button type="submit" className="button">Quero ficar informado!</button>
                     </Fade>
-                </form> */}
+                </form>
             </section>
             <section className="cursos">
 
