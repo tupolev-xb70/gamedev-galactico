c591c75ed85c16ef213b5ccc5a533fe27ffaa9ba
Mathews Motta <mathewscsmotta@gmail.com>
2020-07-26 18:07:41 +0100

    added github pages

--- gdg-frontend/src/pages/Home/index.js
+++ gdg-frontend/src/pages/Home/index.js
@@ -3,6 +3,7 @@ import Jump from 'react-reveal/Jump';
 import Fade from 'react-reveal/Fade';
 
 import './styles.css'
+import logo from '../../assets/gameDevGalacticoNome.png'
 
 // import gamedevImg from '../../assets/gamedev.png'
 
@@ -12,22 +13,24 @@ export default function Home(){
             <section className="form">
                 <div className="logo-text">
                     <Jump>
-                        <h1>GameDev Galático</h1>
+                        <img src={logo} alt="GameDev Galáctico"/>
+                        {/* <h1>GameDev Galáctico</h1> */}
                     </Jump>
                 </div>
                 <div className="desc-text">
                     <Fade bottom>
-                        <p>Aprenda a fazer jogos com guias de altíssima qualidade! Tudo sobre design, programação e design de jogos.</p>
+                        <p>Aprenda a fazer jogos com guias de altíssima qualidade!</p> 
+                        <p>Tudo sobre design, programação e design de jogos.</p>
                     </Fade>
                 </div>
                 
-                <form>
+                {/* <form>
                     <Fade bottom>
                         <h1 className="form-desc">Receba um E-Book Grátis</h1>
                         <input placeholder="Seu E-mail" />
                         <button type="submit" className="button">Receber E-Book!</button>
                     </Fade>
-                </form>
+                </form> */}
             </section>
             <section className="cursos">
 
