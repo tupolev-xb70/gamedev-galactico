c09ca4bdbf672fb783d510ef3993795b6cfd7d85
Mathews Motta <mathewscsmotta@gmail.com>
2020-07-26 17:53:31 +0100

    Updates

---
+++ precache-manifest.f402328dd3d3257a99c0cb91d10306c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a1b1949bea3b1854f98960519721cfc",
+    "url": "/gamedev-galactico/index.html"
+  },
+  {
+    "revision": "f3ddb66407253e800d60",
+    "url": "/gamedev-galactico/static/css/main.06a6b66d.chunk.css"
+  },
+  {
+    "revision": "e9ff2c97f6d51b3dc4d4",
+    "url": "/gamedev-galactico/static/js/2.0c9a6ef0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gamedev-galactico/static/js/2.0c9a6ef0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3ddb66407253e800d60",
+    "url": "/gamedev-galactico/static/js/main.aec1f9f4.chunk.js"
+  },
+  {
+    "revision": "4bebe47bfc3d8cc99de7",
+    "url": "/gamedev-galactico/static/js/runtime-main.b9c4c267.js"
+  }
+]);
